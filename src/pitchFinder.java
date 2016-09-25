37e15884d2fd93e76b22c9947e48f9a6ae354c55
theunbearable <sean1517@comcast.net>
2016-09-24 21:57:42 -0500

    Replaced interval ratio lookup from array with manual calculation inline.

--- src/pitchFinder.java
+++ src/pitchFinder.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 
 public class pitchFinder
 {
-    private static final double[] ratios = {0.5946035, 0.6299605, 0.66742, 0.707107, 0.7491535, 0.7937005,
-            0.8408965, 0.8908985, 0.9438745, 1.0, 1.059463, 1.122462};
     private static final double REFERENCE = 440;
 
     private static final double REFERENCE_OCTAVE = 4;
 
     public static void main(String[] args)
     {
-        while(true)
+        while (true)
         {
             System.out.println("Please enter the note you'd like to see the pitch of: e.g. A_4");
 
@@ -22,53 +20,51 @@ public class pitchFinder
 
             String noteOctave = note.substring(2);
 
-            double freqMultiple = 0;
+            double numerator = 0;
 
             switch (noteName)
             {
                 case "C_":
-                    freqMultiple = ratios[0];
+                    numerator = 3;
                     break;
                 case "C#":
-                    freqMultiple = ratios[1];
+                    numerator = 4;
                     break;
                 case "D_":
-                    freqMultiple = ratios[2];
+                    numerator = 5;
                     break;
                 case "D#":
-                    freqMultiple = ratios[3];
+                    numerator = 6;
                     break;
                 case "E_":
-                    freqMultiple = ratios[4];
+                    numerator = 7;
                     break;
                 case "F_":
-                    freqMultiple = ratios[5];
+                    numerator = 8;
                     break;
                 case "F#":
-                    freqMultiple = ratios[6];
+                    numerator = 9;
                     break;
                 case "G_":
-                    freqMultiple = ratios[7];
+                    numerator = 10;
                     break;
                 case "G#":
-                    freqMultiple = ratios[8];
+                    numerator = 11;
                     break;
                 case "A_":
-                    freqMultiple = ratios[9];
+                    numerator = 0;
                     break;
                 case "A#":
-                    freqMultiple = ratios[10];
+                    numerator = 1;
                     break;
                 case "B_":
-                    freqMultiple = ratios[11];
+                    numerator = 2;
                     break;
             }
 
-            double octaveMultiple = 0;
+            double octaveMultiple = Math.pow(2, Double.parseDouble(noteOctave) - REFERENCE_OCTAVE);
 
-            octaveMultiple = Math.pow(2, Double.parseDouble(noteOctave) - REFERENCE_OCTAVE);
-
-            double frequency = REFERENCE * freqMultiple * octaveMultiple;
+            double frequency = Math.pow(2, numerator / 12) / 2 * REFERENCE * octaveMultiple;
 
             System.out.println("Note frequency is: " + frequency);
         }
